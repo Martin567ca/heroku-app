b0586c36b6de116f865c11c03991cb589ded56cb
JustTheTip21 <martin4o_1995@hotmail.com>
2021-01-31 23:39:27 +0100

    test

--- back/server.js
+++ back/server.js
@@ -12,9 +12,9 @@ dotenv.config()
 mongoose.connect(process.env.MONGODB_URI || process.env.DATABASE_ACCESS, ()=>console.log("Database connected"))
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('/front/build'))
+    app.use(express.static('../build'))
     app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname,'front','build','index.html'));
+        res.sendFile(path.join(__dirname,'../','build','index.html'));
     })
 }
 
